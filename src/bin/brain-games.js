b716cfd70aebe9a6df124ffb81862d5eeb4be73d
Nata Guseva <nataguseva@list.ru>
2020-01-21 06:38:31 +0400

    step3

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
+import { hello } from '..';
 console.log('Welcome to the Brain Games!');
+console.log(hello());
---
+++ src/hello.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+export const hello = () => {
+const userName = readlineSync.question('May I have your name?');
+return `Hello, ${userName}!`;
+};
