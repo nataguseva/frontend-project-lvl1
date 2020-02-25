dd84dd0705671d406a303c7544ad7d4665ab15f5
Nata Guseva <nataguseva@list.ru>
2020-02-25 21:00:16 +0400

    Работа над ошибками 6.2

--- src/games/progressionGame.js
+++ src/games/progressionGame.js
@@ -7,7 +7,7 @@ const progressionsLength = 10;
 const getGameSet = () => {
   const start = getRandomNumber(1, 10);
   const diff = getRandomNumber(1, 10);
-  const indexOfMissingNumber = getRandomNumber(1, progressionsLength);
+  const indexOfMissingNumber = getRandomNumber(0, progressionsLength - 1);
   let question = '';
   const rightAnswer = start + diff * indexOfMissingNumber;
   for (let i = 0; i <= progressionsLength - 1; i += 1) {
